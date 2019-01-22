7c2fee63fabd5b7ec1c8994764a114284dbfbb88
JasonASC4 <jasonhr781@gmail.com>
2019-01-22 09:14:16 -0500

    Create Partial.java

---
+++ Partial.java
@@ -0,0 +1,22 @@
+class Partial {
+  public static void main(String[] args) {
+    Partial p = new Partial();
+    p.partial(null, "a", 1);
+  
+  }
+  public String partial(String original, String letter, int count) {
+    if (original == null) {
+      System.out.println("null");
+      return null;
+    } else  {
+        int sub = original.indexOf(letter);
+        if (sub == -1)  {
+          System.out.println("\"\"");
+          return ("\"\"");
+        } else  {
+          String subb = original.substring(sub, sub + count + 1);
+          System.out.println(subb);
+          return subb;
+        }
+    }
+  }
